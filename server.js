f51b1081230944c0f9f3bf7443d7e02aaf9feb0b
Fernando Roa <froao@unal.edu.co>
2023-10-07 19:45:49 -0300

    chore: dependency config first commit

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/subscribers')
+app.listen(3000, () => console.log("Server Started"));
