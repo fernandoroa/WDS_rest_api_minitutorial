2867c4b3a170869717513efe2a8b4e71b233594e
Fernando Roa <froao@unal.edu.co>
2023-10-07 20:12:56 -0300

    chore: add basic code for routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-module.exports = router
+// get all
+router.get("/", (req, res) => {});
+
+// get one
+router.get("/:id", (req, res) => {});
+
+// create one
+router.post("/", (req, res) => {});
+
+// updating one
+router.patch("/:id", (req, res) => {});
+
+// delete one
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
