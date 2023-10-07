1292d862b874c16ec76033c73a1b256039602851
Fernando Roa <froao@unal.edu.co>
2023-10-07 20:07:30 -0300

    chore: add file subscribers for Router

--- server.js
+++ server.js
@@ -6,5 +6,8 @@ mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("connected to db"));
+app.use(express.json());
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
 
 app.listen(3000, () => console.log("Server Started"));
