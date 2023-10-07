4bbf9138fe3056acdb602d1953dce71fdee74e27
Fernando Roa <froao@unal.edu.co>
2023-10-07 19:54:18 -0300

    chore: add db logs

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/subscribers')
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/subscribers");
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected to db"));
+
 app.listen(3000, () => console.log("Server Started"));
