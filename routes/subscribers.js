b5b597c5867c69d4c7f1020d669caabbc45087c5
Fernando Roa <froao@unal.edu.co>
2023-10-08 19:26:54 -0300

    feat: add Subscriber model

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
-
+const Subscriber = require("../models/Subscriber");
 // get all
-router.get("/", (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // get one
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
 
 // create one
 router.post("/", (req, res) => {});
