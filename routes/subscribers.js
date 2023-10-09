1b444c47c81474e50d209e75377e92349d57d3cd
Fernando Roa <froao@unal.edu.co>
2023-10-09 08:42:06 -0300

    feat: add the patch method logic

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -31,7 +31,20 @@ router.post("/", async (req, res) => {
 });
 
 // updating one
-router.patch("/:id", getSubscriber, (req, res) => {});
+router.patch("/:id", getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // delete one
 router.delete("/:id", getSubscriber, async (req, res) => {
